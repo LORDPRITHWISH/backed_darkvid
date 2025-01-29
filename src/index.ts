41b4fd195b3f17ae44f99a7b1d5773447da1309d
LORDPRITHWISH <prithwishchakraborty05@gmail.com>
2025-01-29 11:36:18 +0530

    midst of creation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+// import dotenv from "dotenv";
+// import express from "express";
+import { app } from "./app";
+import connectDB from "./db";
+
+// dotenv.config({
+//   path: "./.env",
+// });
+console.log(Bun.env.PORT);
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+connectDB()
+  .then(()=>{
+  app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
+  })
+  .catch((error) => {
+    console.error("DB connection error", error);
+    process.exit(1);
+  });
